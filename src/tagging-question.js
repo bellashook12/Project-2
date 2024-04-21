809528ca35d0f4b6e28e30fbbfd21ea0accf4e16
bellashook12 <bellakshook@gmail.com>
2024-04-21 19:42:04 -0400

    4/21

--- src/tagging-question.js
+++ src/tagging-question.js
@@ -11,7 +11,7 @@ export class TaggingQuestion extends DDD {
   constructor() {
     super();
     this.question = "Which of the following big ideas would YOU associate with this artistic work?";
-    //this.image = "";  
+    this.image = "https://private-user-images.githubusercontent.com/329735/320881436-b1e940c9-b838-4e0f-acfb-2d354926bc8d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMyODUzOTcsIm5iZiI6MTcxMzI4NTA5NywicGF0aCI6Ii8zMjk3MzUvMzIwODgxNDM2LWIxZTk0MGM5LWI4MzgtNGUwZi1hY2ZiLTJkMzU0OTI2YmM4ZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNDE2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDQxNlQxNjMxMzdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wZTc4YjA1NWZhYmE0MWNiNWVkOGFiNmZhZTU4Nzg3NDJmOTQxYmNhY2MwMDg3YWNhMDQyYzQ5N2NjN2E1NzI1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.YVplTmO04YCMAImA0GGmFG5bU2UBpI-I1Cy2LoR7bPs";    
 
     this.options = ["Good Form", "Poor Taste", "Contrasting Themes", "AI", "Shading", "Original Work", "Accessible"];
     this.correctAnswers = [""];
@@ -32,7 +32,7 @@ export class TaggingQuestion extends DDD {
     return css`
       :host {
         display: flex;
-        padding: 24px;
+        padding: var(--ddd-spacing-4);
        // align-items: center;
        // justify-content: center;
         
@@ -40,9 +40,9 @@ export class TaggingQuestion extends DDD {
 
       .background{
         background-color: var(--ddd-theme-default-limestoneMaxLight);
-        padding: 8px;
+        padding: var(--ddd-spacing-3);
         display:flex;
-        height: 1000px;
+        height: 1400px;
         width: 900px;
         flex-direction: column;
       }
@@ -51,48 +51,71 @@ export class TaggingQuestion extends DDD {
         display: flex;
         height: 200px;
         width: 200px;
-        padding: 16px;
+        padding: var(--ddd-spacing-3);
         
         
       }
 
       .question {
         color: black;
-        font-size: 24px;
-        padding: 16px;
+        font-size: 32px;
+        padding: var(--ddd-spacing-3);
       }
 /
 
       .answers-start-box{
-        padding: 16px;
+        padding: var(--ddd-spacing-3);
       }
 
-      .answer-options-box,.answer-drop-box {
+      .answer-options-box {
         font-size: 28px;
         display: inline;
         margin: var(--ddd-spacing-3);
         padding: var(--ddd-spacing-4);
         cursor: pointer;
-        border: solid 3px black;
+        border: solid 3px var(--ddd-theme-default-keystoneYellow);
+        background-color: var(--ddd-theme-default-roarLight);
+
+
         
       }
 
       .answer-drop-box{
-        padding: 12px;
-        border: solid 3px black;
+        background-color: var(--ddd-theme-default-alertNonEmergency);
+        border: dashed 3px var(--ddd-theme-default-forestGreen);
+        font-size: 28px;
+        display: inline;
+        margin: var(--ddd-spacing-3);
+        padding: var(--ddd-spacing-4);
+        cursor: pointer;
+        min-height: 32px;
+       
         display: flex;
         
       }
 
 
       .buttons {
+        //display: flex;
+        padding: var(--ddd-spacing-3);
+        margin: var(--ddd-spacing-3);
+      
+        
+      }
+
+      .buttons button{
+        width: 50px;
+        font-size: 30px;
         display: flex;
         padding: 8px;
-        margin: 8px;
-        padding-top: 32px;
-        size: 24px;
-        font-size: 24px;
+        margin: 16px;
         
+        
+      }
+
+      .nonsense{
+        padding: var(--ddd-spacing-3);
+        font-size: 24px;
       }
       
 
@@ -231,7 +254,9 @@ export class TaggingQuestion extends DDD {
     return html`
     <div class= "wrapper">
       <div class= "background">
+        <div class= "nonsense">Eventually this will be text about whatever picture i choose- This should be a block element that in your demo sits between some text that rambles about the topic in question so it appears more real to the context (this will be used in classes) You're going to make a choice today that will have a direct impact on where you are five years from now. The truth is, you'll make choice like that every day of your life. The problem is that on most days, you won't know the choice you make will have such a huge impact on your life in the future. So if you want to end up in a certain place in the future, you need to be careful of the choices you make today.There was a time in his life when her rudeness would have set him over the edge. He would have raised his voice and demanded to speak to the manager. That was no longer the case. He barely reacted at all, letting the rudeness melt away without saying a word back to her. He had been around long enough to know where rudeness came from and how unhappy the person must be to act in that way. All he could do was feel pity and be happy that he didn't feel the way she did to lash out like that.</div>
         <div id= "image-slot" ></div>
+        <img class = "image" src= ${this.image} >
 
 
           <div class="question">Question: ${this.question}</div>
