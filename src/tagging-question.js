b9bc66926139f6ede6e8c518c1c9c0473a92ee3d
bellashook12 <bellakshook@gmail.com>
2024-04-21 19:13:18 -0400

    4/21

--- src/tagging-question.js
+++ src/tagging-question.js
@@ -1,4 +1,4 @@
-import { html, css } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 
 
@@ -11,9 +11,20 @@ export class TaggingQuestion extends DDD {
   constructor() {
     super();
     this.question = "Which of the following big ideas would YOU associate with this artistic work?";
-    this.image = "https://private-user-images.githubusercontent.com/329735/320881436-b1e940c9-b838-4e0f-acfb-2d354926bc8d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMyODUzOTcsIm5iZiI6MTcxMzI4NTA5NywicGF0aCI6Ii8zMjk3MzUvMzIwODgxNDM2LWIxZTk0MGM5LWI4MzgtNGUwZi1hY2ZiLTJkMzU0OTI2YmM4ZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNDE2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDQxNlQxNjMxMzdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wZTc4YjA1NWZhYmE0MWNiNWVkOGFiNmZhZTU4Nzg3NDJmOTQxYmNhY2MwMDg3YWNhMDQyYzQ5N2NjN2E1NzI1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.YVplTmO04YCMAImA0GGmFG5bU2UBpI-I1Cy2LoR7bPs";  
+    //this.image = "";  
+
+    this.options = ["Good Form", "Poor Taste", "Contrasting Themes", "AI", "Shading", "Original Work", "Accessible"];
+    this.correctAnswers = [""];
+    this.draggedIndex;
+    this.draggedFrom;
+    this.hintText = "Drag Correct Answers Here";
+
+    this.checked = false;
+    this.answerSet = "default";
     
-     
+    
+    
+
   }
 
   
@@ -31,7 +42,7 @@ export class TaggingQuestion extends DDD {
         background-color: var(--ddd-theme-default-limestoneMaxLight);
         padding: 8px;
         display:flex;
-        height: 600px;
+        height: 1000px;
         width: 900px;
         flex-direction: column;
       }
@@ -50,45 +61,225 @@ export class TaggingQuestion extends DDD {
         font-size: 24px;
         padding: 16px;
       }
-
-      .answers-drop-box{
-        padding: 16px;
-        
-      }
+/
 
       .answers-start-box{
         padding: 16px;
       }
 
+      .answer-options-box,.answer-drop-box {
+        font-size: 28px;
+        display: inline;
+        margin: var(--ddd-spacing-3);
+        padding: var(--ddd-spacing-4);
+        cursor: pointer;
+        border: solid 3px black;
+        
+      }
+
+      .answer-drop-box{
+        padding: 12px;
+        border: solid 3px black;
+        display: flex;
+        
+      }
+
+
+      .buttons {
+        display: flex;
+        padding: 8px;
+        margin: 8px;
+        padding-top: 32px;
+        size: 24px;
+        font-size: 24px;
+        
+      }
+      
+
+
+
   `;
   }
+
+ // check() {  
+
+//   fetch('answers.json')
+//   .then(response => response.json())
+//         .then(data => {
+//           console.log(data);
+//             const correctAnswers = data.haxLogo;
+
+//             const feedbacks = [];
+//             this.correctAnswers.forEach(answer => {
+//                 if (correctAnswers.hasOwnProperty(answer)) {
+//                     if (correctAnswers[answer].correct) {
+//                         feedbacks.push(correctAnswers[answer].feedback);
+//                     }
+//                 }
+//             });
+
+//             if (feedbacks.length === 0) {
+//                 alert("None of the answers are correct!");
+//             } else {
+//                 feedbacks.forEach(feedback => {
+//                     alert(feedback);
+//                 });
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error fetching JSON file: ', error);
+//         });
+// }
+
+
+  firstUpdated() {
+    super.firstUpdated();
+    const answerBoxes = this.shadowRoot.querySelectorAll('.answer-drop-box');
+    const questionBoxes = this.shadowRoot.querySelectorAll('.answer-options-box');
+
+    answerBoxes.forEach(answerBox => {
+      answerBox.addEventListener('dragstart', (e) => this.dragStart(e));
+      answerBox.addEventListener('dragover', (e) => this.dragOver(e));
+      answerBox.addEventListener('dragenter', (e) => this.dragEnter(e));
+      answerBox.addEventListener('dragleave', (e) => this.dragLeave(e));
+      answerBox.addEventListener('drop', (e) => this.drop(e, 'answer-drop-box'));
+    });
+
+    questionBoxes.forEach(questionBox => {
+      questionBox.addEventListener('dragstart', (e) => this.dragStart(e));
+      questionBox.addEventListener('dragover', (e) => this.dragOver(e));
+      questionBox.addEventListener('dragenter', (e) => this.dragEnter(e));
+      questionBox.addEventListener('dragleave', (e) => this.dragLeave(e));
+      questionBox.addEventListener('drop', (e) => this.drop(e, 'answer-options-box'));
+    });
+  }
+
+  dragOver(e) {
+    e.preventDefault();
+  }
+
+  dragStart(e) {
+    this.draggedIndex = parseInt(e.target.dataset.index);
+    this.draggedFrom = e.target.dataset.origin;
+  }
+
+  dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('hovered');
+  }
+
+  dragLeave(e) {
+    e.target.classList.remove('hovered');
+  }
+
+  drop(e, target) {
+    e.preventDefault();
+
+    e.target.classList.remove('hovered');
+
+    if (target === 'answer-drop-box') {
+      if (this.draggedFrom != 'answer-drop-box') {
+        this.correctAnswers.push(this.options[this.draggedIndex]);
+        this.options.splice(this.draggedIndex, 1);
+      }
+    } else if (target === 'answer-options-box') {
+      if (this.draggedFrom != 'answer-options-box') {
+        this.options.push(this.correctAnswers[this.draggedIndex]);
+        this.correctAnswers.splice(this.draggedIndex, 1);
+      }
+    }
+    this.hintTextCheck();
+
+    this.requestUpdate();
+  }
+
+  hintTextCheck() {
+    if (this.correctAnswers == '') {
+      this.hintText = "Drag Correct Answers Here";
+    } else {
+      this.hintText = "";
+    }
+
+    this.requestUpdate();
+  }
+
+  clear() {
+    if (this.correctAnswers != '') {
+
+      this.correctAnswers.forEach(answer => {
+        this.options.push(answer);
+      });
+
+      this.correctAnswers = [];
+      
+    }
+    this.requestUpdate();
+  }
+
+  check() {
+    if (this.correctAnswers != '') {
+      
+    }
+    this.requestUpdate();
+  }
+
+
+
 
   
   render() {
     return html`
     <div class= "wrapper">
       <div class= "background">
-        <img class= "image" src= ${this.image} >
+        <div id= "image-slot" ></div>
+
+
           <div class="question">Question: ${this.question}</div>
-            <div class="answers-drop-box">Drop Answers Here</div>
-              <div class="answers-start-box">Drag These Answers</div>
+          <div class="answer-space">
+              <div class="answer-drop-box">
+                ${this.hintText}
+                ${this.correctAnswers.map((answer, index) => html`
+                  <div class="answers-wrapper">
+                    <div class="correctAnswers" draggable="true" data-index="${index}" data-origin="answer-drop-box">${answer}</div>
+                  </div>
+                `)}
+              </div>
 
+              <div class="buttons">
+                <button @click="${this.clear}" class="clear-btn">Reset</button>
+                <button @click="${this.check}" class="check-btn">Check</button>
+              </div>
+            </div>
 
-        
+          <div class="answer-options-box">
+            ${this.options.map((question, index) => html`
+              <div class="choices-wrapper">
+                <div class="choices" draggable="true" data-index="${index}" data-origin="answer-options-box">${question}</div>
+              </div>
+            `)}
+          </div>
+        </div>
       </div>
-    </div>
+              
 
     
     `;
+
+  
 
   }
 
   static get properties() {
     return {
-      question: { type: Array },            
+      options: { type: Array, reflect: true },
+      correctAnswers: { type: Array, reflect: true },
+      draggedIndex: { type: Number, reflect: true },
+      draggedFrom: { type: String, reflect: true},
+      hintText: { type: String, reflect: true},
       
     };
   }
 }
 
 globalThis.customElements.define(TaggingQuestion.tag, TaggingQuestion);
+
